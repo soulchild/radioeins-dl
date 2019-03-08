ac516346f0b01eeaf945324503807747095bd9c5
Tobias Kremer <tobias.kremer@gmail.com>
2019-03-08 15:19:14 +0100

    Auto-fix via eslint

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-'use strict';
-
 const cheerio = require('cheerio');
 const request = require('request');
 const fs = require('fs');
 const { URL } = require('url');
 const EventEmitter = require('events');
 
-const loadURL = url => new Promise((resolve, reject) => request.get(url.toString(), (err, response, body) => {
-  if (err || response.statusCode !== 200) {
-    reject(
-      new Error(`Error fetching URL ${url.toString()} (${err.statusCode || err.message})`),
-    );
-    return;
-  }
-  resolve(body);
-}));
+const loadURL = url =>
+  new Promise((resolve, reject) =>
+    request.get(url.toString(), (err, response, body) => {
+      if (err || response.statusCode !== 200) {
+        reject(
+          new Error(`Error fetching URL ${url.toString()} (${err.statusCode || err.message})`)
+        );
+        return;
+      }
+      resolve(body);
+    })
+  );
 
 class Stream extends EventEmitter {
   constructor(url) {
@@ -28,24 +29,24 @@ class Stream extends EventEmitter {
 
     request
       .get(this.url)
-      .on('response', (res) => {
+      .on('response', res => {
         if (res.statusCode !== 200) {
           throw new Error(`Error downloading stream from ${this.url} (${res.statusCode})`);
         }
         bytesTotal = Number.parseInt(res.headers['content-length'], 10);
         this.emit('response', {
-          bytesTotal,
+          bytesTotal
         });
       })
-      .on('data', (chunk) => {
+      .on('data', chunk => {
         bytesDownloaded += chunk.length;
         this.emit('progress', {
           chunkBytes: chunk.length,
           bytesTotal,
-          bytesDownloaded,
+          bytesDownloaded
         });
       })
-      .on('error', (err) => {
+      .on('error', err => {
         throw new Error(`Error downloading media: ${err.statusCode}`);
       })
       .pipe(fs.createWriteStream(filename));
@@ -63,25 +64,25 @@ class RadioEins {
   }
 
   getInfo() {
-    return loadURL(this.showUrl).then((html) => {
+    return loadURL(this.showUrl).then(html => {
       const $ = cheerio.load(html);
       const playerInfo = JSON.parse(
         $('.playerdownload')
           .children('div[data-jsb]')
           .first()
-          .attr('data-jsb'),
+          .attr('data-jsb')
       );
 
       const url = this.makeAbsoluteURL(playerInfo.media);
-      return loadURL(url).then((res) => {
+      return loadURL(url).then(res => {
         const json = JSON.parse(res);
         // eslint-disable-next-line no-underscore-dangle
         const streams = json._mediaArray[0]._mediaStreamArray.map(
-          stream => new Stream(stream._stream), // eslint-disable-line no-underscore-dangle
+          stream => new Stream(stream._stream) // eslint-disable-line no-underscore-dangle
         );
         return {
           title: json.rbbtitle,
-          streams,
+          streams
         };
       });
     });
